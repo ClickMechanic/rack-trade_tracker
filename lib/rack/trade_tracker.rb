b177b222c1773023ee5d5de334828455bd4ab690
Ben Forrest <ben@clickmechanic.com>
2017-08-17 17:17:00 +0100

    adds domain name requirement

--- lib/rack/trade_tracker.rb
+++ lib/rack/trade_tracker.rb
@@ -1,7 +1,19 @@
 require "rack/trade_tracker/version"
+require "rack/trade_tracker/parameters"
 
 module Rack
-  module TradeTracker
-    # Your code goes here...
+  class TradeTracker
+    
+    InitializationError = Class.new(RuntimeError)
+    
+    def initialize(app, options = {})
+      @app = app
+      @domain_name = options[:domain_name]
+      raise InitializationError.new('options must include :domain_name') unless @domain_name
+    end
+    
+    private
+    
+    attr_reader :domain_name
   end
 end
--- spec/rack/trade_tracker_spec.rb
+++ spec/rack/trade_tracker_spec.rb
@@ -1,11 +1,15 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe Rack::TradeTracker do
-  it "has a version number" do
+  let(:app) { ->(env) { [200, env, 'app'] } }
+  
+  subject { Rack::TradeTracker.new(app, domain_name: 'test.com') }
+  
+  it 'has a version number' do
     expect(Rack::TradeTracker::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'requires a domain name' do
+    expect { Rack::TradeTracker.new(app) }.to raise_error(Rack::TradeTracker::InitializationError)
   end
 end
