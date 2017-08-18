4d15a6bfa056c4680b11ae382229f6c455ee67eb
Ben Forrest <ben@clickmechanic.com>
2017-08-18 18:10:51 +0100

    refactors handlers_spec to use shared examples

--- spec/rack/trade_tracker/handler_spec.rb
+++ spec/rack/trade_tracker/handler_spec.rb
@@ -26,9 +26,7 @@ RSpec.describe Rack::TradeTracker::Handler do
                                                                    path: '/',
                                                                    domain: domain} }
 
-      context 'with paired parameters' do
-        let(:params) { 'campaignID=ABC123&redirectURL=www.your-proper-url.com' }
-
+      shared_examples 'redirects with cookie' do
         it 'redirects to the trackback URL' do
           status, headers, body = subject.call(env)
           expect(status).to eq 301
@@ -41,51 +39,41 @@ RSpec.describe Rack::TradeTracker::Handler do
           status, headers, body = subject.call(env)
           expect(headers['set-cookie']).to eq "#{cookie.name}=#{cookie_value}"
         end
+      end
+
+      shared_examples 'missing redirect URL' do
+        it 'redirects to root with 302' do
+          status, headers, body = subject.call(env)
+          expect(status).to eq 302
+          expect(headers['Location']).to eq 'http://www.example.com'
+        end
+
+        it 'does not forward the request' do
+          expect(app).not_to receive(:call)
+          subject.call(env)
+        end
+      end
+
+      context 'with paired parameters' do
+        include_examples 'redirects with cookie' do
+          let(:params) { 'campaignID=ABC123&redirectURL=www.your-proper-url.com' }
+        end
 
         context 'with missing redirect URL' do
-          let(:params) { 'campaignID=ABC123' }
-
-          it 'redirects to root with 302' do
-            status, headers, body = subject.call(env)
-            expect(status).to eq 302
-            expect(headers['Location']).to eq 'http://www.example.com'
-          end
-
-          it 'does not forward the request' do
-            expect(app).not_to receive(:call)
-            subject.call(env)
+          include_examples 'missing redirect URL' do
+            let(:params) { 'campaignID=ABC123' }
           end
         end
       end
 
       context 'with delimited parameters' do
-        let(:params) { 'tt=ABC123_123456&r=www.your-proper-url.com' }
-
-        it 'redirects to the trackback URL' do
-          status, headers, body = subject.call(env)
-          expect(status).to eq 301
-          expect(headers['Location']).to eq Rack::TradeTracker::TRACKBACK_URL
-        end
-
-        it 'creates the Trade Tracker cookie' do
-          allow(Rack::TradeTracker::Cookie).to receive(:new).and_return(cookie)
-          cookie_value = "123456%3A%3AABC123%3A%3Aref; domain=test.com; path=/; expires=#{Rack::Utils.rfc2822(cookie.as_hash[:expires].utc)}"
-          status, headers, body = subject.call(env)
-          expect(headers['set-cookie']).to eq "#{cookie.name}=#{cookie_value}"
+        include_examples 'redirects with cookie' do
+          let(:params) { 'tt=ABC123_123456&r=www.your-proper-url.com' }
         end
 
         context 'with missing redirect URL' do
-          let(:params) { 'tt=ABC123_123456' }
-
-          it 'redirects to root with 302' do
-            status, headers, body = subject.call(env)
-            expect(status).to eq 302
-            expect(headers['Location']).to eq 'http://www.example.com'
-          end
-
-          it 'does not forward the request' do
-            expect(app).not_to receive(:call)
-            subject.call(env)
+          include_examples 'missing redirect URL' do
+            let(:params) { 'tt=ABC123_123456' }
           end
         end
       end
